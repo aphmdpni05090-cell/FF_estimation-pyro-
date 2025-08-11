ad79a89bc7ae5f14b7d7edb625da18efcbfc7d32
aphmdpni05090-cell <aphmdpni.05090@gmail.com>
2025-08-11 16:46:13 +0200

    Update Deep_datapreprocess.py

--- Deep_datapreprocess.py
+++ Deep_datapreprocess.py
@@ -3,11 +3,11 @@ import pandas as pd
 data= pd.read_csv("extra_vcf.tsv", sep="\t")
 data=data.shift(periods=1)
 data.iloc[0,:]=data.columns
-data.columns=("Chr","Pos" ,"Ref","Alt","AF","DP","AC")
+data.columns=("Chr","Pos" ,"Ref","Alt","DP","AC")
 
-for col in ["AF","DP","AC"]:
+for col in ["DP","AC"]:
          data[col] = pd.to_numeric(data[col], errors='coerce')
-data["AF"] = data.apply(lambda x: x["AC"] / x["DP"] if x["DP"] != 0 else 0, axis=1)
+data["VAF"] = data.apply(lambda x: x["AC"] / x["DP"] if x["DP"] != 0 else 0, axis=1)
 df=pd.read_csv("insert_size_ranksum.cleaned.tsv", sep="\t")
 #split_df = df["Variant_ID"].str.extract(r'(?P<Chr>[^:]+):(?P<pos>\d+):(?P<Ref>[^>]+)>(?P<Alt>.+)')
 
@@ -24,9 +24,10 @@ data["Pos"] = data["Pos"].astype(int)
 
 df_merge=pd.merge(df,data,how='left',on=["Chr","Pos" ,"Ref","Alt"])
 DP_data=df_merge.iloc[:,3:]
-DP_data.rename(columns={'AC':'alt_count','AF':'vaf','Z_score':'frag_stat','DP':'depth'},inplace=True)
+DP_data.rename(columns={'AC':'alt_count','VAF':'vaf','Z_score':'frag_stat','DP':'depth'},inplace=True)
 DP_data['frag_stat']=df_merge["Z_score"]
 DP_data.to_csv("/DP_variantsrunFF.tsv",sep="\t", index=False)
+
 
 
 
